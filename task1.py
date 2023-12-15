e90299682602d1d40a48f31427359462e4539995
redcommander561 <126519219+redcommander561@users.noreply.github.com>
2023-12-15 12:33:54 -0800

    done

--- task1.py
+++ task1.py
@@ -7,3 +7,47 @@ Create entry widets to allow user to enter their:
 
 Create a button so that when they click on the button, it states all of the information in a 4th entry widget
 """
+
+
+import tkinter as tk
+from tkinter import *
+
+win = tk.Tk()
+win.geometry("600x200")
+
+
+
+
+l1 = Label(win,text="name:")
+l2 = Label(win,text="student number:")
+l3 = Label(win,text="grade:")
+e1 = Entry(win,width=20)
+e2 = Entry(win,width=20)
+e3 = Entry(win,width=20)
+b1 = Button(win,text="Click to return information to database")
+
+
+
+
+def clickFunction(event):
+    print(event)
+    name = e1.get()
+    name = str(name)
+    stnum = e2.get()
+    stnum = float(stnum)
+    grade = e3.get()
+    grade = float(grade)
+    print(name, stnum,grade)
+b1.bind("<Button>",clickFunction)
+
+l1.grid(row=1,column=1)
+e1.grid(row=1,column=2)
+l2.grid(row=1,column=3)
+e2.grid(row=1,column=4)
+l3.grid(row=1,column=5)
+e3.grid(row=1,column=6)
+b1.grid(row=2,column=1,columnspan=6)
+
+
+
+win.mainloop()
--- task2.py
+++ task2.py
@@ -6,3 +6,43 @@ The first 2 entry widgets allow the user to enter in the 2 short sides of a righ
 When the button is clicked, calculate the length of the hypotenuse and display it in the 3rd entry widget.
 Any labels you need for instruction are optional.
 """
+
+import tkinter as tk
+from math import sqrt
+
+def calculate_hypotenuse():
+    side1 = float(s1_entry.get())
+    side2 = float(s2_entry.get())
+    
+    
+    hypotenuse = sqrt(side1**2 + side2**2)
+
+    
+    result_entry.delete(0, tk.END)
+    result_entry.insert(0, f"Hypotenuse: {hypotenuse:.2f}")
+
+
+app = tk.Tk()
+app.title("Right Triangle Hypotenuse Calculator")
+
+
+s1label = tk.Label(app, text="Enter side 1:")
+s1label.grid(row=0, column=0, padx=10, pady=10)
+s1_entry = tk.Entry(app)
+s1_entry.grid(row=0, column=1, padx=10, pady=10)
+
+s2_label = tk.Label(app, text="Enter side 2:")
+s2_label.grid(row=1, column=0, padx=10, pady=10)
+s2_entry = tk.Entry(app)
+s2_entry.grid(row=1, column=1, padx=10, pady=10)
+
+
+calculate = tk.Button(app, text="Calculate Hypotenuse", command=calculate_hypotenuse)
+calculate.grid(row=2, column=0, columnspan=2, pady=10)
+
+
+result_entry = tk.Entry(app, width=30)
+result_entry.grid(row=3, column=0, columnspan=2, pady=10)
+
+
+app.mainloop()
